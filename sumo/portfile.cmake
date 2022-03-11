e490e809ce160837f65dcb8fc9cb7232b265e89f
Tim <tim@fake.WHAT>
2022-03-11 15:41:05 -0500

    fixed debug build (find proj lib correctly)

--- sumo/portfile.cmake
+++ sumo/portfile.cmake
@@ -4,8 +4,11 @@ vcpkg_from_github(
     REF 800cb422fa4b9107a1d799d73fc19dbdd7f2cdab
     SHA512 175b1f2fdac230ccdfd78fba1c8147f870db4fda05194521dcc95f0c97d9815b63b956c2347a218acefbf2af1e6558690104fde1a41d2a1a17971941b3cdb5a3
     HEAD_REF master
+	PATCHES
+		0001-vcpkg-build.patch
 )
 
+#set(SOURCE_PATH E:/new_libs/scripts/vcpkg/buildtrees/sumo/src/bdd7f2cdab-3f99139187.clean)
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
 )
@@ -29,6 +32,10 @@ file(GLOB DBG_EXEs ${CURRENT_PACKAGES_DIR}/debug/bin/*.exe)
 file(REMOVE ${DBG_EXEs})
 file(REMOVE ${CURRENT_PACKAGES_DIR}/debug/bin/start-command-line.bat)
 
+#no clue how these even get to here??
+file(REMOVE ${CURRENT_PACKAGES_DIR}/debug/bin/libsumocpp.dll)
+file(REMOVE ${CURRENT_PACKAGES_DIR}/debug/bin/libtracicpp.dll)
+
 #unfortunately, sumo provides a folder with the same name as "lib" that contains "site packages". im not sure the best way to do this,
 #but the lib folder is where import libraries should be
 file(RENAME "${CURRENT_PACKAGES_DIR}/Lib/site-packages" "${CURRENT_PACKAGES_DIR}/site-packages")
@@ -42,6 +49,8 @@ file(RENAME "${CURRENT_PACKAGES_DIR}/bin/libtracicpp.lib" "${CURRENT_PACKAGES_DI
 file(RENAME "${CURRENT_PACKAGES_DIR}/bin/start-command-line.bat" "${CURRENT_PACKAGES_DIR}/tools/sumo/start-command-line.bat")
 file(RENAME "${CURRENT_PACKAGES_DIR}/debug/bin/libsumocppD.lib" "${CURRENT_PACKAGES_DIR}/debug/lib/libsumocppD.lib")
 file(RENAME "${CURRENT_PACKAGES_DIR}/debug/bin/libtracicppD.lib" "${CURRENT_PACKAGES_DIR}/debug/lib/libtracicppD.lib")
+
+#why am i finding release lbs in the debug folder always?
 
 #empty
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/tools/contributed/saga" "${CURRENT_PACKAGES_DIR}/tools/contributed/traci4matlab")
